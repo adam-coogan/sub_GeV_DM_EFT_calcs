ecb2bef6dc2142459ed796bbd0b08d8bdc7a8098
Adam Coogan <acoogan@ucsc.edu>
2018-07-11 18:49:22 -0700

    Fixed mass typo.

--- Simplified model amplitudes/Pseudoscalar mediator/python/dwidth_ds_p_to_pi0pipi.py
+++ Simplified model amplitudes/Pseudoscalar mediator/python/dwidth_ds_p_to_pi0pipi.py
@@ -1,6 +1,6 @@
 (Sqrt(s*(-4*mpi**2 + s))*Sqrt(mp**4 + (mpi0**2 - s)**2 - 
-      2*mp**2*(mpi0**2 + s))*(beta**2*(2*mpi**2 + mpi0 - 3*s)**2*vh**2 + 
-      2*b0*beta*(2*mpi**2 + mpi0 - 3*s)*vh*
+      2*mp**2*(mpi0**2 + s))*(beta**2*(2*mpi**2 + mpi0**2 - 3*s)**2*vh**2 + 
+      2*b0*beta*(2*mpi**2 + mpi0**2 - 3*s)*vh*
        (-(beta*(mdq + muq)*vh) + fpi*(gpGG*(mdq - muq) + (gpdd - gpuu)*vh)) \
 + b0**2*(beta**2*(mdq + muq)**2*vh**2 - 
          2*beta*fpi*(mdq + muq)*vh*(gpGG*(mdq - muq) + (gpdd - gpuu)*vh) - 

0d897ba3bf2ae4ac4a17a016b55153b0d16b7380
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2018-08-15 21:05:20 -0700

    Updated the python files for scalar mediator widths and cross sections

--- Simplified model amplitudes/Scalar mediator/python/width_s_to_pipi.py
+++ Simplified model amplitudes/Scalar mediator/python/width_s_to_pipi.py
@@ -1,6 +1,10 @@
-(Sqrt(-4*mpi**2 + ms**2)*(-54*gsGG*(2*mpi**2 - ms**2)*vh*
-        (3*vh + 3*gsff*vs + 2*gsGG*vs) + 
-       b0*(mdq + muq)*(9*vh + 4*gsGG*vs)*
-        (54*gsGG*vh - 32*gsGG**2*vs + 9*gsff*(9*vh + 16*gsGG*vs)))**2)/
-  (11664.*ms**2*Pi*vh**2*(3*vh + 3*gsff*vs + 2*gsGG*vs)**2*
-    (9*vh + 4*gsGG*vs)**2)
+((sqrt(-4 * mpi**2 + ms**2) *
+  ((gsGG * (4 * mpi**2 - 2 * ms**2)) /
+   (9 * Lam + 4 * gsGG * vs) +
+   (b0 * (mdq + muq) *
+    (27 * gsff**2 * Lam**2 * vs * (3 * Lam + 4 * gsGG * vs) -
+     2 * gsGG * vh**2 *
+     (27 * Lam**2 -
+      30 * gsGG * Lam * vs + 8 * gsGG**2 * vs**2) +
+     gsff * (-81 * Lam**3 * vh + 48 * gsGG**2 * Lam * vh * vs**2))) /
+   (81. * Lam**3 * vh**2))**2) / (16. * ms**2 * pi))
--- Simplified model amplitudes/Scalar mediator/python/xsec_xx_s_gg.py
+++ Simplified model amplitudes/Scalar mediator/python/xsec_xx_s_gg.py
@@ -1,2 +1,2 @@
-(alphaEM**2*gsFF**2*gsxx**2*Q**4*Sqrt(1 - 4*rx**2))/
-  (256.*Pi**3*vh**2*(ms**4 + Q**4 + ms**2*(-2*Q**2 + widths**2)))
+((alphaEM**2 * gsFF**2 * gsxx**2 * Q**4 * np.sqrt(1 - 4 * rx**2)) /
+    (64. * Lam**2 * Pi**3 * (ms**4 + Q**4 + ms**2 * (-2 * Q**2 + widths**2))))

ad57f9e140c8d635fd9ad88867e35d03064efeff
Adam Coogan <dr.adam.coogan@gmail.com>
2019-07-25 14:13:14 +0200

    add x x -> pi0 v cross section

---
+++ Simplified model amplitudes/Vector mediator/python/xsec_xx_v_pi0v.py
@@ -0,0 +1,5 @@
+((gvdd - gvuu)**2*(gvdd + gvuu)**2*gvxx**2*
+    ((-mpi0 - mv + Q)*(mpi0 - mv + Q)*(-mpi0 + mv + Q)*(mpi0 + mv + Q))**
+     1.5*(2*mx**2 + Q**2))/
+  (1536.*fpi**2*Pi**5*Q**3*Sqrt(-4*mx**2 + Q**2)*
+    ((-mv**2 + Q**2)**2 + mv**2*widthv**2))
